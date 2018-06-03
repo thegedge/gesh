d0c61a81e0ca2522caae893cbc244ffd2db0701a
Jason Gedge <jason.gedge@shopify.com>
2018-06-02 23:41:21 -0400

    Refactor so that Shell#parse_line isn't needed

--- src/shell.rs
+++ src/shell.rs
@@ -11,42 +11,54 @@ use readline::{
 };
 
 use parser::{
+    self,
     ParsedLine,
     Parser,
 };
 
+/// A user shell.
+///
 pub struct Shell<R: Reader> {
     pub reader: R,
     pub parser: Parser,
 }
 
-/// Enumeration of all possible errors that can occur in the shell
+/// Enumeration of all possible errors that can occur in the shell.
 ///
 #[derive(Debug)]
 pub enum Error {
     ReadlineError(readline::Error),
+    ParserError(parser::Error),
 }
 
 impl<R: Reader> Shell<R> {
+    /// Runs the shell's main read -> parse -> execute loop.
+    ///
     pub fn run(&mut self) -> Result<(), Error> {
         loop {
-            match self.reader.get() {
-                Ok(raw_line) => self.parse_line(raw_line),
+            let parsed_line = match self.reader.get() {
+                Ok(raw_line) => self.parser.parse(&raw_line)?,
                 Err(readline::Error::Eof()) => break,
+                Err(readline::Error::Interrupted()) => continue,
                 Err(err) => return Err(Error::ReadlineError(err)),
+            };
+
+            match parsed_line {
+                ParsedLine::Command(cmd) => println!("command: {:?}", cmd)
             }
         }
         Ok(())
     }
+}
 
-    fn parse_line(&mut self, raw_line: String) {
-        match self.parser.parse(&raw_line) {
-            Ok(parsed_line) => match parsed_line {
-                ParsedLine::Command(cmd) => println!("command: {:?}", cmd),
-            },
-            Err(err) => {
-                println!("error: {:?}", err)
-            }
-        }
+impl From<readline::Error> for Error {
+    fn from(err: readline::Error) -> Self {
+        Error::ReadlineError(err)
+    }
+}
+
+impl From<parser::Error> for Error {
+    fn from(err: parser::Error) -> Self {
+        Error::ParserError(err)
     }
 }
