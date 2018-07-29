f44ace141721cd118c86ef14ad0ce7f36bb21c83
Jason Gedge <jason.gedge@shopify.com>
2018-07-28 20:09:34 -0400

    Write tests for 'pushd' builtin

--- src/command/builtin/pushd.rs
+++ src/command/builtin/pushd.rs
@@ -9,11 +9,43 @@ use command::{
 };
 
 pub fn pushd(Context { env, args }: Context) -> Result {
-    match args.into_iter().next() {
-        Some(dir) => {
-            env.push_directory(PathBuf::from(dir));
+    match args.len() {
+        0 => {
+            // TODO swap top two paths
+            Ok(ExitStatus::Success(1))
+        },
+        1 => {
+            // TODO check for existence of path before pushing
+            env.push_directory(PathBuf::from(&args[0]));
             Ok(ExitStatus::Success(0))
         },
-        None => Ok(ExitStatus::Success(1)),
+        _ => Ok(ExitStatus::Success(1)),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use environment::Environment;
+
+    #[test]
+    fn test_pushd_adds_given_directory_to_stack() {
+        let env = &mut Environment::empty();
+        let args = vec!["./src".to_owned()];
+
+        let result = pushd(Context { env, args });
+
+        assert_eq!(Ok(ExitStatus::Success(0)), result);
+        assert_eq!(Some(&PathBuf::from("./src")), env.directory_stack().last());
+    }
+
+    #[test]
+    fn test_pushd_returns_error_with_too_many_arguments() {
+        let env = &mut Environment::empty();
+        let args = vec!["a".to_owned(), "b".to_owned(), "c".to_owned()];
+
+        let result = pushd(Context { env, args });
+
+        assert_eq!(Ok(ExitStatus::Success(1)), result);
     }
 }
